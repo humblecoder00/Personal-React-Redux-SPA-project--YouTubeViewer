b0c8de4eb085232bae31ed7fe5d849f51fe51345
Şahin Arslan <sahin__arslan@hotmail.com>
2019-08-18 14:23:38 +0200

    initial commit

---
+++ src/apis/youtube.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const KEY = "AIzaSyCy4L1XS-J9g6YVJx8GH39G4LQ7TsuYI6E";
+
+export async function getYoutubeData(term) {
+  const response = await axios.get("/search", {
+    baseURL: "https://www.googleapis.com/youtube/v3",
+    params: {
+      part: "snippet",
+      maxResults: 5,
+      key: KEY,
+      q: term
+    }
+  });
+  return response;
+}
